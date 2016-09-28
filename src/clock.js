7f7a775f73e33dd683e14f643850ce404b22765b
egmfilho <edumig12@hotmail.com>
2016-09-28 08:51:13 -0300

    ver 0.1

---
+++ src/clock.js
@@ -0,0 +1,34 @@
+/**
+ * Created by egmfilho on 20/09/16.
+ */
+
+(function() {
+
+'use strict';
+
+	angular.module('egmfilho.clock', [ ])
+		.directive('customClock', ['$timeout', function ($timeout) {
+
+			function controller($scope) {
+				$scope.relogio = 'carregando relógio';
+
+				function tick() {
+					$scope.relogio = Date.now();
+					$timeout(tick, 1000);
+				}
+
+				$timeout(tick, 1000);
+			}
+
+			return {
+				restrict: 'E',
+				scope: {
+          format: '='
+				},
+				controller: controller,
+				template: '<span>{{relogio | date: (format || "medium")}}</span>'
+			}
+
+		}]);
+
+}());
